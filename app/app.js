d18fb5a5c59162ebfb239439d961f03763a11d48
KevinLi0711 <kli30@stuy.edu>
2023-05-31 16:53:20 -0400

    remove ejs

--- app/app.js
+++ app/app.js
@@ -2,20 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//like jinja
-app.set('view engine', 'ejs')
-
 //run this file using nodemon, which enables auto-reload when changing files
 //npm install nodemon
 //npx nodemon app.js
 
 app.get('/', (req, res) => {
-  res.render('index', {text : 'Hello Never!'})
+  res.sendFile('index.html', {root : 'views'})
   //req.body.<key>
 })
 
 app.post('/', (req, res) => {
-    res.render('index', {text : 'Hello Never!'})
+    res.sendFile('index.html', {root : 'views'})
 })
 
 app.listen(port, () => {
