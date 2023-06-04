131b363b67d580b308cbed5bd1b8efcb1778c0e5
KevinLi0711 <kli30@stuy.edu>
2023-06-04 10:46:54 -0400

    socket testing

--- app/static/javascript/socket.js
+++ app/static/javascript/socket.js
@@ -1,22 +1,33 @@
 var socket = io();
 
 socket.on('connect', function() {
-    console.log("connected")
-    socket.emit('my event', {data: 'I\'m connected!'});
+    //log the id of the socket that connected
+    console.log(socket.id + " connected")
+    //send an event to the server with some data attached
+    var roomsList = document.getElementById('room')
+    newRoom = document.createElement('li')
+    newRoom.innerHTML = ` ${socket.id}`
+    roomsList.appendChild(newRoom)
 });
 
-socket.on('setId', function(id) {
-    idElement = document.getElementById('id')
-    idElement.innerHTML = id
+//when the server sends an update_rooms message, update the rooms list
+socket.on('update_rooms', function(room) {
+    var roomsList = document.getElementById('room')
+    console.log(roomsList)
+    newRoom = document.createElement('li')
+    newRoom.innerHTML = ` ${room}`
+    roomsList.appendChild(newRoom)
 })
 
-function add1() {
-    element = document.getElementById('display')
-    number = element.innerHTML
-    number = Number(number)
-    number += 1
-    element.innerHTML = number
+function joinRoom() {
+    roomId = document.getElementById('roomId').value
+    socket.emit('join_room', socket.id, roomId)
 }
+
+function roomExists(room) {
+    console.log(socket.io.socket.rooms)
+}
+
 
 
 
