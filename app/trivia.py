e4cb548c9a9a5af8fbb78e18bc4ec764d4dcd9a0
Kevin Xiao <78610793+FamousMrChair@users.noreply.github.com>
2023-06-05 12:48:07 -0400

    Update trivia.py

--- app/trivia.py
+++ app/trivia.py
@@ -6,13 +6,18 @@ def get_question():
     r = requests.get(API_URL) #creating a response object that will get us the information we needr
     api_dict = r.json() #r.json() returns a dictonary after deconding the response object
     id = (api_dict[0])["id"]
-    
     question = (api_dict[0])["question"]['text']
     correctAnswer= (api_dict[0])["correctAnswer"]
-    incorrectAnswers = ''
+    incorrectAnswers = []
     for i in range(len((api_dict[0])["incorrectAnswers"])):
-        incorrectAnswers = incorrectAnswers + (api_dict[0])["incorrectAnswers"][i] + ', '
-    return id + ' \n' + question + ' \n' + correctAnswer + '\n' + incorrectAnswers
+        incorrectAnswers = incorrectAnswers.append('(api_dict[0])["incorrectAnswers"][i]' + ', ')
+    dict = {}
+    dict['id'] = id
+    dict['question'] = question
+    dict['correctAnswer'] = correctAnswer
+    dict['incorrectAnswers'] = incorrectAnswers
+    
+    return dict
 
 for i in range(20):
     print(get_question())
