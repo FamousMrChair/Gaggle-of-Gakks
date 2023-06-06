8c0c48535c60a1ba654c1a8c51da338ebe192780
FamousMrChair <kxiao30@stuy.edu>
2023-06-06 13:00:22 -0400

    added a broken minesweeper ig

--- app/minesweeper.py
+++ app/minesweeper.py
@@ -1,14 +1,15 @@
-import requests
+import http.client
 
-url = "https://minesweeper1.p.rapidapi.com/boards/new"
-
-querystring = {"r":"1","c":"1","bombs":"1"}
+conn = http.client.HTTPSConnection("minesweeper1.p.rapidapi.com")
 
 headers = {
-	"X-RapidAPI-Key": "SIGN-UP-FOR-KEY",
-	"X-RapidAPI-Host": "minesweeper1.p.rapidapi.com"
+    'X-RapidAPI-Key': "955171d53fmshdbf33da3892fb58p14c3dcjsn9cf0c834973d",
+    'X-RapidAPI-Host': "minesweeper1.p.rapidapi.com"
 }
 
-response = requests.get(url, headers=headers, params=querystring)
+conn.request("GET", "/boards/new?r=1&c=1&bombs=1", headers=headers)
 
-print(response.json())
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
