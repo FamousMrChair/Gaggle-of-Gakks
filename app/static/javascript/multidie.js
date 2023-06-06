49341b3efb1d88020ba2de76d65d900984b1037a
Anjini Katari <akatari30@stuy.edu>
2023-06-06 00:10:03 -0400

    shee

---
+++ app/static/javascript/multidie.js
@@ -0,0 +1,24 @@
+// inner workings of multiplication to the death 
+
+function random_item(items){
+    return items[Math.floor(Math.random() * items.length)];   
+}
+
+function multiply() {
+    const nums = []
+    for(let i = 0; i <= 12; i++){
+        nums.push(i);
+    }
+    console.log(nums);
+    const a = random_item(nums);
+    console.log(a);
+    const b = random_item(nums);
+
+    var numbers = document.getElementById("numbers");
+    var AHead = document.createElement("h4");
+    AHead.innerHTML = a;
+    numbers.append(AHead);
+
+}
+
+multiply();
