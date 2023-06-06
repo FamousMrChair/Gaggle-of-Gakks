b62549a49ae24e50eb9d87b1534cc6ffbe65a0c3
Anjini Katari <akatari30@stuy.edu>
2023-06-06 13:19:46 -0400

    progresss

--- app/static/javascript/multidie.js
+++ app/static/javascript/multidie.js
@@ -4,23 +4,36 @@ function random_item(items){
     return items[Math.floor(Math.random() * items.length)];   
 }
 
+const nums = []
+for(let i = 0; i <= 12; i++){
+    nums.push(i);
+}
+console.log(nums);
+const a = random_item(nums);
+console.log(a);
+const b = random_item(nums);
+
+
 function multiply() {
-    const nums = []
-    for(let i = 0; i <= 12; i++){
-        nums.push(i);
-    }
-    console.log(nums);
-    const a = random_item(nums);
-    console.log(a);
-    const b = random_item(nums);
-
-    var numbers = document.getElementById("numbers");
-    var AHead = document.createElement("h4");
+    //displaying numbers 
+    numbers = document.getElementById("numbers");
+    AHead = document.createElement("h4");
     AHead.innerHTML = a;
-    numbers.append(AHead);
-
-    
-
+    BHead = document.createElement("h4");
+    BHead.innerHTML = b;
+    numbers.appendChild(AHead);
+    numbers.appendChild(BHead);
 }
 
-multiply();
+function check(){
+    console.log(a * b)
+    if ((a * b) == document.getElementById("ans").value){
+        console.log("yay")
+    }
+    else{
+        console.log("boooo")
+    }
+};
+
+multiply()
+check()
