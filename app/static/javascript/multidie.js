4064223b17641b6780c6ba504ba7501de743fbf4
Anjini Katari <akatari30@stuy.edu>
2023-06-07 13:58:53 -0400

    only timer and clearing textbox left

--- app/static/javascript/multidie.js
+++ app/static/javascript/multidie.js
@@ -8,42 +8,49 @@ const nums = [];
 for(let i = 0; i <= 12; i++){
     nums.push(i);
 }
-const a = random_item(nums);
-const b = random_item(nums);
+var a;
+var b;
 var results = 0;
+AHead = document.createElement("h4");
+BHead = document.createElement("h4");
 
-function multiply() {
-    //displaying numbers 
+
+function multiply(event) {
+    //displaying numbers
+    a = random_item(nums);
+    b = random_item(nums);
     numbers = document.getElementById("numbers");
-    AHead = document.createElement("h4");
     AHead.innerHTML = a;
-    BHead = document.createElement("h4");
     BHead.innerHTML = b;
     numbers.appendChild(AHead);
     numbers.appendChild(BHead);
 }
 
-// function isKeyPressed(event) {
-//     var x = document.getElementById("demo");
-//     if (event.shiftKey) {
-//       x.innerHTML = "The SHIFT key was pressed!";
-//     } else {
-//       x.innerHTML = "The SHIFT key was NOT pressed!";
-//     }
-//   }
+function isKeyPressed(event) {
+    var x = document.getElementById("demo");
+    if (event.shiftKey) {
+      x.innerHTML = "The SHIFT key was pressed!";
+    } else {
+      x.innerHTML = "The SHIFT key was NOT pressed!";
+    }
+  }
 
 function check(event){
-    // var x = document.getElementById("demo");
-    // if (event.shiftKey) {
-    //   x.innerHTML = "The SHIFT key was pressed!";
-    // }
-    // console.log(a * b);
-    if ((a * b) == document.getElementById("ans").value){
-        console.log("yay");
-        results += 1;
-    }
-    else{
-        console.log("boooo");
+    var x = document.getElementById("results");
+    if (event.shiftKey) {   
+        if ((a * b) == document.getElementById("ans").value){
+            console.log("yay");
+            results += 1;
+            AHead.innerHTML = "";
+            BHead.innerHTML = "";
+            multiply()
+        }
+        else{
+            console.log("boooo");
+            AHead.innerHTML = "";
+            BHead.innerHTML = "";
+            multiply()
+        }
     }
 };
 
