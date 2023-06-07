1cf9a2e75a0ec671a1db98b6b7d829d3ab11542a
Anjini Katari <akatari30@stuy.edu>
2023-06-07 12:44:06 -0400

    its working a little more

--- app/static/javascript/multidie.js
+++ app/static/javascript/multidie.js
@@ -4,15 +4,13 @@ function random_item(items){
     return items[Math.floor(Math.random() * items.length)];   
 }
 
-const nums = []
+const nums = [];
 for(let i = 0; i <= 12; i++){
     nums.push(i);
 }
-console.log(nums);
 const a = random_item(nums);
-console.log(a);
 const b = random_item(nums);
-
+var results = 0;
 
 function multiply() {
     //displaying numbers 
@@ -25,15 +23,29 @@ function multiply() {
     numbers.appendChild(BHead);
 }
 
-function check(){
-    console.log(a * b)
+// function isKeyPressed(event) {
+//     var x = document.getElementById("demo");
+//     if (event.shiftKey) {
+//       x.innerHTML = "The SHIFT key was pressed!";
+//     } else {
+//       x.innerHTML = "The SHIFT key was NOT pressed!";
+//     }
+//   }
+
+function check(event){
+    // var x = document.getElementById("demo");
+    // if (event.shiftKey) {
+    //   x.innerHTML = "The SHIFT key was pressed!";
+    // }
+    // console.log(a * b);
     if ((a * b) == document.getElementById("ans").value){
-        console.log("yay")
+        console.log("yay");
+        results += 1;
     }
     else{
-        console.log("boooo")
+        console.log("boooo");
     }
 };
 
-multiply()
-check()
+multiply();
+
